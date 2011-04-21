899d670d2aceb64c9b4b155c604803569b01807c
Tapio Vierros <tapiovierros@gmail.com>
2011-04-21 15:38:03 +0300

    Remove connect() (not needed for magic slot names).

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->treeWidget, SIGNAL(customContextMenuRequested(const QPoint&)), this, SLOT(on_treeWidget_customContextMenuRequested(const QPoint&)));
 }
 
 MainWindow::~MainWindow()
